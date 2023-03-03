5cb4f9bab900dc6a37d0683df19e3638263794b5
Mohamed A. Eljaouadi <eljaouadi1mohamed@gmail.com>
2023-03-03 19:26:14 +0100

    remove dump data

--- app/controllers/home.controller.php
+++ app/controllers/home.controller.php
@@ -11,11 +11,6 @@ class Home extends Controller {
 
     public function index ( ) {
 
-        // LOAD USER MODEL
-        $user =  $this->load_model( 'user' );
-
-        print_r ( $user->get_all( ) );
-
         // LOAD VIEW PAGE
         $this->view( 'home' );
         
