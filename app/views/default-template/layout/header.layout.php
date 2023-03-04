463401cab095660a9493b4e4c9d296af211d7c92
Mohamed A. Eljaouadi <eljaouadi1mohamed@gmail.com>
2023-03-04 09:15:41 +0100

    Add Core css style file

--- app/views/default-template/layout/header.layout.php
+++ app/views/default-template/layout/header.layout.php
@@ -6,6 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Dyno PHP Framework</title>
+        <link rel="stylesheet" href="<?= CSS_DIR_PATH ?>core.css">
     </head>
 
     <body>
