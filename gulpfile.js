8a803c3952f959a94ce3d3efe5b60f60fafbf656
Mohamed A. Eljaouadi <eljaouadi1mohamed@gmail.com>
2023-03-04 12:19:48 +0100

    Gulp Task: Define assets  Task

--- gulpfile.js
+++ gulpfile.js
@@ -22,6 +22,8 @@ const { init, write } = sourceMaps;
 
 import optimizeImages from 'gulp-image';
 
+import merge from 'gulp-merge';
+
 //-----------------------------------
 //        SET FRONTEN ROUTES      ---
 //-----------------------------------
@@ -113,6 +115,40 @@ const fonts = (done) => {
   done();
 };
 
+//---------------------------------------
+//             ASSETS TASK            ---
+//---------------------------------------
+
+const assets = async (done) => {
+  // BULMA
+  const bulma = src(`${node_modules}bulma/*.sass`)
+    .pipe(sass().on('Error', sass.logError))
+    .pipe(dest(`${production.css}/assets/`));
+
+  // FONTAWESOME
+  const lineawesome_css = src(
+    `${node_modules}line-awesome/dist/line-awesome/css/line-awesome.css`
+  ).pipe(dest(`${production.fonts}/lineawesome/css`));
+
+  //WEBFONTS DIR
+  const webfonts = src(
+    `${node_modules}line-awesome/dist/line-awesome/fonts/*`
+  ).pipe(dest(`${production.fonts}/lineawesome/fonts`));
+
+  //HTML5SHIV.JS
+  const HTML5shiv = src(`${node_modules}html5shiv/dist/html5shiv.min.js`).pipe(
+    dest(`${production.js}/assets`)
+  );
+  //RESPOND.JS
+  const respond = src(`${node_modules}respond.js/dest/respond.min.js`).pipe(
+    dest(`${production.js}/assets`)
+  );
+
+  return merge(bulma, lineawesome_css, webfonts, HTML5shiv, respond);
+
+  done();
+};
+
 //---------------------------------------------
 //   SETUP DEVELOPMENT TASK  ( WATCH TASK)  ---
 //---------------------------------------------
@@ -133,5 +169,6 @@ task('css', buildCSS);
 task('js', buildJS);
 task('images', compressImages);
 task('fonts', fonts);
-task('watch', parallel(start_server, dev));
-export default parallel(start_server, dev);
+task('assets', assets);
+task('watch', parallel(start_server, assets, dev));
+export default parallel(start_server, assets, dev);
