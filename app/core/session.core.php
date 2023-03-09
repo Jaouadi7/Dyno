eeaaad1bb4853ebae583ff0abf0ea8268c80ee74
Mohamed A. Eljaouadi <eljaouadi1mohamed@gmail.com>
2023-03-09 09:08:48 +0100

    SESSION: Add unset function to remove spesific data from array

--- app/core/session.core.php
+++ app/core/session.core.php
@@ -39,8 +39,8 @@ class Session {
 
 	}
 
-      // SET AND CREATE SPECIFIC DATA TO SESSION
-      public function set ( $data = [ ] ) {
+    // SET AND CREATE SPECIFIC DATA TO SESSION
+    public function set ( $data = [ ] ) {
 
 
         // CHECK DATA IS FOUND AND TYPE IS ARRAY
@@ -56,6 +56,45 @@ class Session {
            
         endif;
 
+    }
+
+     // UNSET SPECIFIC DATA FROM SESSION
+     public function unset( $key ) {
+
+        // TO CHECK IF $_SESSION EXISTS
+		$this->start();
+
+        switch ( $key ) {
+
+            case $this->mainkey: 
+
+                if ( ! empty( $_SESSION[ $this->mainkey ] ) ) :
+
+                    unset( $_SESSION[ $this->mainkey ] );
+
+                endif;
+
+                break;
+
+            case $this->secondrykey:
+
+                if ( ! empty( $_SESSION[ $this->mainkey ][ $this->secondrykey ] ) ) : 
+
+                    unset( $_SESSION[ $this->mainkey ][ $this->secondrykey ] );
+
+                endif;
+
+                break;
+
+            default:
+
+                if ( ! empty( $_SESSION[ $this->mainkey ][ $this->secondrykey ][ $key ] ) ) : 
+
+                    unset( $_SESSION[ $this->mainkey ][ $this->secondrykey ][ $key ] );
+
+                endif;
+
+        }
     }
 
 }
